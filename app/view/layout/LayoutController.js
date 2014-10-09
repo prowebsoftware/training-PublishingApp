47b3bd636065fc3928251017ce43bb59f5533a3e
ashleycoker <ashley.coker@prowebsofwtare.net>
2014-10-09 11:38:29 +0100

    tag 1 after 2!

--- app/view/layout/LayoutController.js
+++ app/view/layout/LayoutController.js
@@ -1,18 +1,5 @@
 Ext.define('Publishing.view.layout.LayoutController', {
     extend: 'Ext.app.ViewController',
-    alias: 'controller.layout-layout',
-
-    routes: {
-        'home' : 'onHome',
-        'users' : 'onUsers'
-    },
-
-    onHome : function(){
-        console.log('home');
-    },
-
-    onUsers : function(){
-        console.log('users');
-    }
+    alias: 'controller.layout-layout'
     
 });
