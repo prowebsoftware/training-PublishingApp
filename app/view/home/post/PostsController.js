7e42d199b4d1b35637edbd75c33d3cc51479eda9
ashleycoker <ashley.coker@prowebsofwtare.net>
2014-10-13 09:11:15 +0100

    13 - Navigation and grid selection (with new/save/delete buttons)

--- app/view/home/post/PostsController.js
+++ app/view/home/post/PostsController.js
@@ -6,15 +6,60 @@ Ext.define('Publishing.view.home.post.PostsController', {
 
     ],
 
+    routes: {
+        'home' : 'onHome',
+        'home/:id' : 'onHome',
+        'users' : 'onUsers'
+    },
+
+    init: function(){
+
+        this.getView().on('render', function(){
+            this.layout = this.getView().findParentByType('layout');
+            this.editform = this.layout.down('editform');
+            this.commentsgrid = this.layout.down('comments');
+            this.postsgrid = this.layout.down('posts');
+        }, this, {single: true});
+
+    },
+
+    onHome : function( id ){
+        var store = this.getStore('posts'),
+            me = this;
+
+        if ( id && store && store.getRange().length==0 ){
+            store.on('load', function(){
+                var record = store.getById(id);
+                if ( record ) {
+                    me.loadRecord(record);
+                }
+            }, this, {single: true});
+        }else if (id){
+            var record = store.getById(id);
+            if ( record ) {
+                me.loadRecord(record);
+            }
+        }else{
+            this.editform.getController().onNew();
+        }
+
+
+    },
+
     onRowClick: function( table, record, tr, rowIndex ){
-        var form = this.getView().findParentByType('layout').down('editform');
+        this.loadRecord( record );
+        this.redirectTo('home/'+record.get('id'));
+    },
 
-        form.getViewModel().set( 'record', record );
-        form.getForm().loadRecord( record );
+    loadRecord: function( record ){
 
-        var grid = this.getView().findParentByType('layout').down('comments');
+        this.editform.getViewModel().set( 'record', record );
+        this.editform.getForm().loadRecord( record );
+
         Publishing.model.Comment.getProxy().setUrl('http://localhost:3000/posts/'+record.get('id')+'/comments');
-        grid.getStore().load();
+        this.commentsgrid.getStore().load();
+
+        this.postsgrid.getSelectionModel().select(record);
     }
     
 });
